d0cd668eed9af57cd0209198225e16339fc14f0c
Vikas Verma <vikas.on@gmail.com>
2026-08-12 12:03:13 +0700

    two-sum.py

---
+++ two-sum.py
@@ -0,0 +1,26 @@
+'''
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    https://leetcode.com/problems/two-sum/description/?envType=problem-list-v2&envId=oizxjoit
+'''
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        for i, a in enumerate(nums):
+            prev = target - a
+            if prev in prevMap :
+                return [prevMap[prev], i] 
+            prevMap[a] = i
+        return;
+
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([3,2,4], 6))
+
+
+
+
+        
